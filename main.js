b2ba90834ed977d3740cf9407153a8f9abac5ef2
Titou-Dh <titoudh1@gmail.com>
2024-03-11 23:16:27 +0100

    init

---
+++ main.js
@@ -0,0 +1,8 @@
+let pic =document.querySelector('.cls');
+let container = document.querySelector('.container') ;
+function phones (phone){
+    pic.src= phone;
+}
+function colors (color){
+    container.style.background = color;
+}
